507385207948a31b0a343b964cb27df902916908
Kuna Yaswanth <94559394+yaswanthksas@users.noreply.github.com>
2023-03-27 20:45:56 +0530

    exported some modules

--- mern_stack_car_share/backend/models/carModel.js
+++ mern_stack_car_share/backend/models/carModel.js
@@ -4,8 +4,11 @@ const carSchema = new mongoose.Schema({
 
     name : {type : String , required : true} ,
     image : {type : String , required : true} , 
+    
     capacity : {type : Number , required : true},
+    
     fuelType : {type : String , required : true} , 
+    
     bookedTimeSlots : [
         {
             from : {type : String , required : true},
@@ -13,11 +16,17 @@ const carSchema = new mongoose.Schema({
         }
     ] , 
 
+    
     rentPerHour : {type : Number , required : true}
 
 
 }, {timestamps : true}
 
+                                      
 )
 const carModel = mongoose.model('cars' , carSchema)
+
 module.exports = carModel
+
+
+
